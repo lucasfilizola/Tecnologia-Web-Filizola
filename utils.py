e3c8b0f42156845fac541da8cc62212fc8ce5851
Lucas Filizola <lucasfpp@al.insper.edu.br>
2024-02-15 10:44:06 -0600

    oi

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+
+import json
 def extract_route(string):
     lista= string.split()
     string_i=lista[1]
@@ -10,3 +12,20 @@ def read_file(file_path):
     with open(file_path,'rb') as arquivo:
         leitura= arquivo.read()
     return leitura
+
+def load_data(file_name):
+
+    # Abre o arquivo JSON e carrega o conteúdo como um objeto Python
+    with open( "data/"+ file_name, 'r') as file:
+        # esse metodo loads pega o que tinha no arquivo json  e transforma em um objeto python
+        content= file.read()
+        data = json.loads(content)
+    
+    return data
+
+def load_template(file_name):
+    with open("templates/" + file_name, 'r') as file:
+        template = file.read()
+    return template
+
+
---
+++ views.py
@@ -0,0 +1,31 @@
+from utils import load_data, load_template
+
+def index(request):
+    
+    # CONTINUANDO AGORA HANDOUT-4
+    # Cria uma lista de <li>'s para cada anotação
+    # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+        for chave_valor in corpo.split('&'):
+            
+
+
+    note_template = load_template('components/note.html')
+    notes_li = [
+        note_template.format(title=dados['titulo'], details=dados['detalhes'])
+        for dados in load_data('notes.json')
+    ]
+    notes = '\n'.join(notes_li)
+
+    return load_template('index.html').format(notes=notes).encode()
+
